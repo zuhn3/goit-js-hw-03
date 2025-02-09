15f537f4c8c19a9d9491b28fa3465bb944a3dc86
zuhn3 <aop1603@gmail.com>
2025-02-09 23:39:28 +0000

    Add files via upload

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+    const newArray = [];
+  
+    for (const num of numbers) {
+      if (num > value) {
+          newArray.push(num);
+      }
+    }
+    return newArray;
+  }
+  
+  console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+  console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+  console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+  console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+  console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
